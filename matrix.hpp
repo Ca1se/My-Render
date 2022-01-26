7385232fbf64d018d381ef9b74281ba77fa8800e
Ca1se <irstCa1se@outlook.com>
2022-01-27 00:14:58 +0800

    2022.1.25

--- matrix.hpp
+++ matrix.hpp
@@ -12,28 +12,16 @@
 template <typename T, size_t row, size_t col>
 class Matrix {
 public:
-    Matrix(): data_((T*) malloc(sizeof(T) * (row * col))) {}
+    Matrix() = default;
+    ~Matrix() = default;
 
     Matrix(std::initializer_list<T> elements) {
         assert((row * col) == elements.size());
-        data_ = (T*) malloc(sizeof(T) * elements.size());
         std::copy(elements.begin(), elements.end(), data_);
     }
 
-    Matrix(const Matrix<T, row, col>& other):
-            data_((T*) malloc(sizeof(T) * other.size())) {
+    Matrix(const Matrix<T, row, col>& other) {
         std::copy(other.begin(), other.end(), data_);
-    }
-
-    Matrix(Matrix<T, row, col>&& other) {
-        data_ = other.data_;
-        other.data_ = nullptr;
-    }
-
-    ~Matrix() {
-        if(data_) {
-            delete [] data_;
-        }
     }
 
     typedef T value_type;
@@ -58,7 +46,7 @@ public:
     const_iterator end() const noexcept { return (data_ + size()); }
 
 private:
-    T* data_;
+    T data_[row * col] = {0};
 };
 
 #endif
--- test.cpp
+++ test.cpp
@@ -4,8 +4,7 @@
 
 int main() {
     Matrix<float, 3, 3> m{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Matrix<float, 3, 3> m1 = std::move(m);
-    for(auto it: m1) {
+    for(auto it: m) {
         std::cout << it << std::endl;
     }
     return 0;
