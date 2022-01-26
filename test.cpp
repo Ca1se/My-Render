8f06896e98069965cad84e3d8480194fb607c926
Ca1se <irstCa1se@outlook.com>
2022-01-27 00:15:15 +0800

    2022.1.27

--- test.cpp
+++ test.cpp
@@ -3,9 +3,6 @@
 #include "matrix.hpp"
 
 int main() {
-    Matrix<float, 3, 3> m{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for(auto it: m) {
-        std::cout << it << std::endl;
-    }
+    
     return 0;
 }
