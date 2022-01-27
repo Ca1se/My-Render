107723a25d89adfd8c530cef33c4f0ba6b3010fc
Ca1se <irstCa1se@outlook.com>
2022-01-27 23:04:02 +0800

    2022.1.27

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
-#include <utility>
 #include "matrix.hpp"
+#include <iostream>
+
 
 int main() {
-    
+    Matrix<float, 2, 3> A;
+    Matrix<float, 3, 3> B;
+    A << 1, 2, 3,
+         4, 5, 6;
+    B << 100, 10, 1,
+         10, 100, 1,
+         1, 10, 100;
+    std::cout << (A * B) << std::endl;
     return 0;
 }
