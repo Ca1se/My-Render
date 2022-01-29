89c389bbd434073916829689346d7a608e566fbc
Ca1se <irstCa1se@outlook.com>
2022-01-29 23:02:16 +0800

    2022.1.29

--- math_utils.hpp
+++ math_utils.hpp
@@ -1,3 +1,11 @@
+// Author: Wu Zihao                 
+// Github: https://github.com/Ca1se 
+//
+// This is a simple linear algebra library containing both 
+// matrix and vector parts. This library is used for 
+// calculations for my computer graphics practice project.
+//
+
 #ifndef _MATH_UTILS_HPP_
 #define _MATH_UTILS_HPP_
 
@@ -15,11 +23,16 @@ inline constexpr bool eq(size_t t1, size_t t2) {
     return (t1 == t2);
 }
 
+// Class Matrix (and Vector) only accept types 
+// that can be used for arithmetic.
 template <typename _T, size_t _Rows, size_t _Cols,
         bool = eq(_Rows, _Cols),
         bool = std::is_arithmetic<_T>::value>
 class Matrix;
 
+// The Base class, This class contains functions 
+// that are available when the number of rows and 
+// columns of the matrix are equal and when they are not.
 template <typename _T, size_t _Rows, size_t _Cols>
 class Matrix<_T, _Rows, _Cols, false, true> {
 public:
@@ -229,6 +242,7 @@ public:
 
 };
 
+// This class is used for comma initialization of matrix
 template <typename _T, size_t _Rows, size_t _Cols>
 class CommaInitializer {
 private:
@@ -271,6 +285,8 @@ inline CommaInitializer<_T, _Rows, _Cols> operator<< (Matrix<_T, _Rows, _Cols>& 
     return CommaInitializer<_T, _Rows, _Cols>(matrix, val);
 }
 
+// This class contains functions that are only available 
+// when the number of rows and the number of cols are equal.
 template <typename _T, size_t _Rows, size_t _Cols>
 class Matrix<_T, _Rows, _Cols, true, true>: public Matrix<_T, _Rows, _Cols, false, true> {
 private:
@@ -307,7 +323,12 @@ public:
 };
 
 
-
+// This class contains functions that are used for 
+// vector operations. 
+// Some functions in this class are unavailable in 
+// some cases, such as function cross is unavailable 
+// when _Size != 3, and if you use it, you will get
+// a compile error.
 template <typename _T, size_t _Size>
 class Vector: public Matrix<_T, _Size, 1> {
 private:
@@ -369,6 +390,7 @@ public:
         return ret;
     }
 
+    // Only available when vector size == 3
     template <typename _U>
     Vector<_T, 3> cross(const Vector<_U, 3>& other_vector) const noexcept {
         static_assert(_Size == 3);
@@ -390,31 +412,37 @@ public:
         return (*this)[0];
     }
 
+    // Only available when vector size > 1
     reference y() noexcept {
         static_assert(_Size > 1);
         return (*this)[1];
     }
 
+    // Only available when vector size > 1
     const_reference y() const noexcept {
         static_assert(_Size > 1);
         return (*this)[1];
     }
 
+    // Only available when vector size > 2
     reference z() noexcept {
         static_assert(_Size > 2);
         return (*this)[2];
     }
 
+    // Only available when vector size > 2
     const_reference z() const noexcept {
         static_assert(_Size > 2);
         return (*this)[2];
     }
 
+    // Only available when vector size > 3
     reference w() noexcept {
         static_assert(_Size > 3);
         return (*this)[3];
     }
 
+    // Only available when vector size > 3
     const_reference w() const noexcept {
         static_assert(_Size > 3);
         return (*this)[3];
