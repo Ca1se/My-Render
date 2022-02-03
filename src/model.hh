01eac101d647b5bd3e62bb58755ccd1e093b8917
Ca1se <irstCa1se@outlook.com>
2022-02-03 23:10:59 +0800

    2022.2.3

--- src/model.hh
+++ src/model.hh
@@ -12,11 +12,8 @@ struct VertexInfo {
     int uv_index;
 };
 
-
 class Model {
 private:
-    static std::string model_dir_path_;
-
     std::vector<Vector3f> vertices_;
     std::vector<Vector3f> normals_;
     std::vector<Vector2f> uv_coords_;
@@ -25,36 +22,31 @@ private:
     std::vector<std::array<VertexInfo, 3>> faces_;
 
 public:
-    static void setModelDirPath(const std::string& path) {
-        model_dir_path_ = path;
-    }
-
-    static void loadModel(const std::string& obj_file_path);
-
-    const std::vector<Vector3f>& get_vertices() const noexcept { return vertices_; }
-    const std::vector<Vector3f>& get_normals() const noexcept { return normals_; }
-    const std::vector<Vector2f>& get_uv_coords() const noexcept { return uv_coords_; }
+    const std::vector<Vector3f>& vertices() const noexcept { return vertices_; }
+    const std::vector<Vector3f>& normals() const noexcept { return normals_; }
+    const std::vector<Vector2f>& uv_coords() const noexcept { return uv_coords_; }
+    const auto& faces() const noexcept { return faces_; }
 
 public:
-    friend class ObjPraser;
+    friend class ObjLoader;
 };
 
-inline std::vector<Model> models;
-inline std::string Model::model_dir_path_ = "./model/";
 
-class ObjPraser {
+class ObjLoader {
 private:
-    std::string_view obj_str_;
+    std::vector<Model> loaded_models_;
+
+    std::string obj_str_;
     size_t now;
     size_t pre;
 
 public:
-    ObjPraser(const std::string& obj_str) noexcept;
-
-public:
-    size_t prase(std::vector<Model>& result, bool recursed = false);
+    ObjLoader& load(const std::string& obj_file_name);
+    const std::vector<Model>& loaded_models() const { return loaded_models_; }
 
 private:
+    void praseObj();
+
     std::string_view nextLine() noexcept;
 
     bool hasNextLine() const noexcept { return now != obj_str_.size(); }
