1b41a32bb497f1d8438932a6251072b419de4823
Ca1se <irstCa1se@outlook.com>
2022-02-10 23:58:54 +0800

    2022.2.10

--- src/material.hh
+++ src/material.hh
@@ -3,22 +3,24 @@
 
 #include <cstdint>
 #include <memory>
+#include "png_image.hh"
 #include "math_utils.hh"
 
 
 class Texture {
 private:
-    size_t width_;
-    size_t height_;
-    bool alpha_;
-    std::shared_ptr<std::uint8_t[]> data_;
+    std::shared_ptr<PNGImage> image_;
 
 public:
     Texture();
     bool loadTexture(const std::string& texture_file_name);
-    Vector<std::uint8_t, 3> get(int u, int v) const noexcept {
-        // unfinished
+    Vector<std::uint8_t, 3> sample(float u, float v) const noexcept {
+        int img_u = u * image_->width();
+        int img_v = (1 - v) * image_->height();
+        auto color = image_->getColor(img_u, img_v);
+        return Vector<std::uint8_t, 3>{color[0], color[1], color[2]};
     }
+
 };
 
 enum IllumMode {
