10e7ca7d0970d5f3ba92b1a76d0bdd8ff3711e6c
Ca1se <irstCa1se@outlook.com>
2022-02-13 22:33:53 +0800

    2022.2.12

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -1,10 +1,45 @@
 #ifndef _PIPELINE_HPP_
 #define _PIPELINE_HPP_
 
-namespace pipeline {
+#include <vector>
+#include "matrix.hpp"
+#include "model.hpp"
+#include "shader.hpp"
+#include "macro.hpp"
 
+enum Plane {
+    MINIMAL,
+    TOP,
+    BOTTOM,
+    RIGHT,
+    LEFT,
+    NEAR,
+    FAR
+};
 
+struct Payload {
+    Vector4f coords[3];
+    Vector3f world_coords[3];
+    Vector3f normals[3];
+    Vector2f uvs[3];
 
-}
+    Vector4f clipped_coords[CLIPPED_MAX];
+    Vector3f clipped_world_coords[CLIPPED_MAX];
+    Vector3f clipped_normals[CLIPPED_MAX];
+    Vector2f clipped_uvs[CLIPPED_MAX];
+};
+
+class Pipeline {
+private:
+    static Payload payload;
+
+public:
+    static void draw(const Model &model, VertexShader shader);
+
+private:
+    static int homogeneous_clip();
+    static int clipWithPlane(Plane clip_plane, int vertex_num);
+};
+
 
 #endif
---
+++ src/shader.hpp
@@ -0,0 +1,14 @@
+#ifndef _SHADER_HPP_
+#define _SHADER_HPP_
+
+#include <functional>
+#include "matrix.hpp"
+
+struct VertexShader {
+    Matrix4f mvp_matrix;
+    Vector4f process(const Vector3f& coord) {
+        return mvp_matrix * Vector4f{ coord.x(), coord.y(), coord.z(), 1 };
+    }
+};
+
+#endif
