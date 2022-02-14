6b55a4f195b914d2fcd804cbfa5e44112af6bc2f
Ca1se <irstCa1se@outlook.com>
2022-02-14 23:54:07 +0800

    2022.2.14

--- src/util.hpp
+++ src/util.hpp
@@ -1,10 +1,17 @@
 #ifndef _UTIL_HPP_
 #define _UTIL_HPP_
 
+#include <cstddef>
 #include "camera.hpp"
 #include "matrix.hpp"
 
 Matrix4f calViewMatrix(const Camera& camera);
 Matrix4f calPerspectiveMatrix(float fov, float aspect, float near, float far);
+
+template <typename T, size_t Size>
+inline Vector<T, Size> vectorInterpolate(const Vector<T, Size>& start, 
+        const Vector<T, Size>& end, float t) noexcept {
+    return start + (end - start) * t;
+}
 
 #endif
