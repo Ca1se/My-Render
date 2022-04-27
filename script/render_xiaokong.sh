8907a8e03a86be2464f629f593cce7e39228a943
caseshot <blame773@gmail.com>
2022-04-27 12:12:08 +0800

    change project name 'render' to 'renderer'

--- script/render_xiaokong.sh
+++ script/render_xiaokong.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-../build/render --model ../example/xiaokong/body.obj ../example/xiaokong/emoji.obj ../example/xiaokong/hair.obj ../example/xiaokong/head.obj ../example/xiaokong/robot.obj --texture ../example/xiaokong/body.png ../example/xiaokong/emoji.png ../example/xiaokong/hair.png ../example/xiaokong/head.png ../example/xiaokong/robot.png
+../build/renderer --model ../example/xiaokong/body.obj ../example/xiaokong/emoji.obj ../example/xiaokong/hair.obj ../example/xiaokong/head.obj ../example/xiaokong/robot.obj --texture ../example/xiaokong/body.png ../example/xiaokong/emoji.png ../example/xiaokong/hair.png ../example/xiaokong/head.png ../example/xiaokong/robot.png
